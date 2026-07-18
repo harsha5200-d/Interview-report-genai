c2b5d532501460ca6a5fef960f2a981ae842a05f
harsha <hreshavena@gmail.com>
2026-07-18 20:45:00 +0530

    Add DNS fix for MongoDB in Backend/server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 const app = require("./src/app");
 const connectDB = require("./src/config/database");
 
